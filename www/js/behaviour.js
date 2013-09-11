c2e4404ad6a1b341469058c3974f2ec742ed7a46
Adam Hewartson <adam@sck-webworks.co.uk>
2013-09-11 12:49:02 +0100

    Change to Invest In Cumbria app

--- www/js/behaviour.js
+++ www/js/behaviour.js
@@ -22,162 +22,38 @@ function onDeviceReady() {
 	states[Connection.CELL_4G]  = 'Cell 4G connection';
 	states[Connection.NONE]     = 'No network connection';
 	
-	var teams = window.localStorage.getItem("team");
-	if(teams) {
-		defaults = $.parseJSON(teams);
+	var news = window.localStorage.getItem("news");
+	if(news) {
+		defaults = $.parseJSON(news);
 	}
 	
-	if(networkState == Connection.WIFI || networkState == Connection.ETHERNET || networkState == Connection.CELL_4G) {
+	if(networkState != Connection.NONE) {
+		$('body').addClass('connected');
 		$.ajax({
-			url:"http://www.sck-webworks.co.uk/behaviour/team.php",
+			url:"http://www.investincumbria.co.uk/news/json.php",
+			dataType:"json",
 			success:function(data) {
-				window.localStorage.removeItem("team");
-				window.localStorage.setItem("team", JSON.stringify(data));
-				
-				if(typeof FileTransfer != 'undefined') {
-					$.each(data,function() {
-						var getimg = "http://www.sck-webworks.co.uk/images/sck-team/"+this.img;
-						var setimg = this.img;
-						saveImage(getimg,setimg,'#'+data.firstName+data.lastName,true);
-					});
-				}
-				
-				buildContent(data);
+				window.localStorage.removeItem("news");
+				window.localStorage.setItem("news", JSON.stringify(data));
+				buildNewsContent(data);
+				resized();
 			},
 			error:function(e) {
-				buildContent(defaults);
+				buildNewsContent(defaults);
 			}
 		});
-		resized();
 	} else {
-		$.each(data,function() {
+		/*$.each(data,function() {
 			var getimg = "images/"+this.img;
 			var setimg = this.img;
 			saveImage(getimg,setimg,'#'+data.firstName+data.lastName,false);
-		});
-		buildContent(defaults);
+		});*/
+		buildNewsContent(defaults);
 	}
 	
-	if(location.hash) {
-		getPageFromHash();
-	}
-	$(window).bind('hashchange', function() {
-		if(navused) {
-			getPageFromHash();
-		}
-	});
-};
-
-function saveImage(getimg,setimg,id,override) {
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-		fileSystem.root.getFile(setimg, {create: true, exclusive: false}, function(fileEntry) {
-			var localPath = fileEntry.fullPath;
-			
-			localpath = localpath.substring(0,localpath.lastIndexOf('/'));
-			localpath = localpath + "sckteam/" + setimg;
-			
-			if (device.platform === "Android" && localPath.indexOf("file://") === 0) {
-				localPath = localPath.substring(7);
-			}
-			
-			var reader = new FileReader();
-			var fileSource = localPath;
-			
-			reader.onloadend = function(evt) {
-				var exists = true;
-				if(evt.target.result == null) {
-				   exists = false;
-				}
-				if(!exists || (exists && override)) {
-					var ft = new FileTransfer();
-					ft.download(getimg, localPath, function(entry) {
-						var d = new Date();
-						$(id).find('.image_cell').find('img').attr("src",localpath + d.getTime());
-					}, fail);
-				} else {
-					$(id).find('.image_cell').find('img').attr("src",localpath + d.getTime());
-				}
-			}
-			reader.readAsDataURL(fileSource);  
-		}, fail);
-	}, fail);
-}
-
-function getPageFromHash() {
-	var myhash = location.hash;
-	if(myhash == "") {
-		myhash = '#home';
-	}
-	if(!$(myhash+':visible').length > 0) {
-		$('.pagelink[href*='+myhash+']:eq(0)').click();
-	}
-}
-
-function buildContent(team) {
-
-	var contacts = team;
-	
-	for(id in contacts) {
-		contact = contacts[id];
-		var licontent = applyTemplate(listtemplate,contact);
-		var pagecontent = applyTemplate(contacttemplate,contact);
-		$('.listview').append(licontent);
-		$('body').append(pagecontent);
-	}
-	
-	var panel = $('.navmenu-panel');
-	
-	$('.page').hide()
-	
-	setTimeout(function () {
-		
-		var options = new ContactFindOptions();
-		options.filter = '';
-		options.multiple = true;
-		if(navigator.contacts) {
-			navigator.contacts.find(["displayName","name","phoneNumbers"],function(result) {
-		
-				$('.page').each(function(i) {
-					var $p = $(this);
-					
-					if($p.attr('data-phone')) {
-						var firstname = $p.attr('data-firstname');
-						var lastname = $p.attr('data-lastname');
-						var phone = $p.attr('data-phone');
-						var email = $p.attr('data-email');
-						var img = new Image();
-						img.src = $p.find('.image_cell').find('img').attr('src');
-						
-						found = false;
-						for(item in result) {
-							var fc = result[item];
-							if(fc.displayName = firstname + " " + lastname || fc.name.givenName == firstname && fc.name.familyName == lastname) {
-								var phonefound = false;
-								if(fc.phoneNumbers) {
-									for(var i = 0; i < fc.phoneNumbers.length; i++) {
-										if(fc.phoneNumbers[i].value == phone) {
-											phonefound = true;
-										}
-									}
-								}
-								if(phonefound) {
-									found = true;
-								}
-							}
-						}
-						if(!found) {
-							addContactButton($p,firstname,lastname,phone, email, getBase64Image(img));
-						} else {
-							$p.find('.actions').append('<a href="" class="addContact btn ui-disabled">Added</a>');
-						}
-					}
-				});
-			},fail,options);
-		}
-	}, 10);
-	
+	$('.page').hide();
+	$('#home').show();
 	$('.navmenu-panel').hide();
-	$('#home').addClass('page');
 	
 	resized();
 	$(window).bind('resize',resized)
@@ -192,26 +68,79 @@ function buildContent(team) {
 		return false;
 	});
 	
-	$('.pagelink').click(function() {
+	setupNavigation($('.page, #navmenu-panel, #footer'));
+	$('.imgback img, .icon img, .bars img, .zoom img').attr('ondragstart','return false');
+	
+	if(location.hash) {
+		getPageFromHash();
+	}
+	$(window).bind('hashchange', function() {
+		if(navused) {
+			getPageFromHash();
+		}
+	});
+};
+var inverse = false; 
+
+function gotoPage(targetid) {
+	$p = $('.page:visible');
+	var st = $(window).scrollTop();
+	if(targetid == '#home' || inverse) {
+		$p.css('top',-st).animate({width:'hide',left:'100%'},400, function() { $(this).css('top',0); });
+		$(targetid).css('left','0%').animate({width:'show'},400);
+		inverse = false;
+	} else {
+		$p.css('top',-st).animate({width:'hide'},400, function() { $(this).css('top',0); });
+		$(targetid).css('left','100%').animate({width:'show',left:'0%'},400);
+	}
+	$('#navmenu-panel').find('li').removeClass('active').find('a[href='+targetid+']').parents('li').addClass('active');
+	if($('#navmenu-panel:visible').length > 0) {
+		$('#navmenu-panel:visible').animate({width:'hide'},400);
+	}
+}
+
+var xhr;
+function getPage(targetid) {
+	var url = targetid.replace('#','');
+	url += ".html";
+	
+	if(xhr) {
+		xhr.abort();
+	}
+	
+	xhr = $.ajax({
+		url:url,
+		dataType:'html',
+		success:function(data) {
+			var $p = $(targetid,data);
+			if($p.length == 0) {
+				$p = $('.page:first',data);
+				$p.attr('id',targetid.replace('#',''));
+			}
+			$('.pages').append($p);
+			setupNavigation($('.pages').children(':last'));
+			resized();
+			gotoPage(targetid);
+		}
+	});
+}
+
+function setupNavigation($p) {
+	$p.find('.pagelink').unbind('click').click(function() {
 		$('.panel-cover').hide();
 		var targetid = $(this).attr('href');
 		visid = $('.page:visible').attr('id');
 		
-		if($(targetid).length > 0 && targetid != '#'+visid) {
-			if(targetid == '#home') {
-				$('.page:visible').animate({width:'hide',left:'100%'},400);
-				$(targetid).css('left','0%').animate({width:'show'},400);
+		if(targetid != '#'+visid) {
+			if($(targetid).length > 0) {
+				gotoPage(targetid);
 			} else {
-				$('.page:visible').animate({width:'hide'},400);
-				$(targetid).css('left','100%').animate({width:'show',left:'0%'},400);
-			}
-			if($('#navmenu-panel:visible').length > 0) {
-				$('#navmenu-panel:visible').animate({width:'hide'},400);
+				getPage(targetid);
 			}
 		}
 	});
 	
-	$('.page').bind('touchstart', function(e) {
+	$p.find('.page').unbind('touchstart').unbind('touchend').unbind('touchmove').bind('touchstart', function(e) {
 		var touch = e.originalEvent.touch || e.originalEvent.touches[0];
 		tstartX = touch.pageX;
 		tstartY = touch.pageY;
@@ -234,25 +163,66 @@ function buildContent(team) {
 				}
 				
 				if(tX < tstartX - 20) {
-					var $n = $p.nextAll('.page:first');
-					if($n.length > 0) {
-						$p.animate({width:'hide'},400);
-						$n.css('left','100%').animate({width:'show',left:'0%'},400);
-					}
+					inverse = true;
+					history.back();
 				}
 				if(tX > tstartX + 20) {
-					var $n = $p.prevAll('.page:first');
-					if($n.length > 0) {
-						$p.animate({width:'hide',left:'100%'},400);
-						$n.css('left','0%').animate({width:'show'},400);
-					}
+					history.forward();
 				}
 			}
 			tstartX = tstartY = tX = tY = false;
 		}
 	});
 	
+	$p.find('.expand_head').click(function() {
+		$a = $(this);
+		$body = $a.next('.expand_body');
+		if($a.hasClass('active')){
+			$body.stop(true,true).animate({'height':'hide'},'slow');
+			$a.removeClass('active');
+		} else {
+			$body.stop(true,true).animate({'height':'show'},'slow');
+			$a.addClass('active');
+		}
+	}).filter(':not(.active)').siblings('.expand_body').hide();
+}
+
+function getPageFromHash() {
+	var myhash = location.hash;
+	if(myhash == "") {
+		myhash = '#home';
+	}
+	if(!$(myhash+':visible').length > 0) {
+		if($(myhash).length > 0) {
+			gotoPage(myhash);
+		} else {
+			getPage(myhash);
+		}
+	}
+}
+
+function buildNewsContent(newsdata) {
+
+	var news = newsdata;
+	
 	$('.loading').remove();
+	$newscont = $('#news').find('.page_content');
+	
+	if(newsdata.length > 0) {
+		$newscont.append('<ul></ul>');
+		for(id in news) {
+			newsitem = news[id];
+			var licontent = applyTemplate(listtemplate,newsitem);
+			var pagecontent = applyTemplate(newstemplate,newsitem);
+			$newscont.children('ul').append(licontent);
+			$('.pages').append(pagecontent);
+			setupNavigation($('.pages').children(':last'));
+		}
+	} else {
+		$newscont.append('Connect to the internet to download the latest news');
+	}
+	setupNavigation($('#news'));
+	
 }
 
 function fail(error) {
@@ -264,41 +234,8 @@ function resized() {
 	$('.nav-container').width($(window).width()*.6).height($(document).height());
 }
 
-function addContactButton(target,firstname,lastname,phone,email,img) {
-	target.find('.actions').append('<a href="" class="addContact btn">Add '+firstname+'</a>');
-	var addLink = target.find('.addContact');
-	addLink.click(function() {
-		var addLink = $(this);
-		
-		var contact = navigator.contacts.create();
-		contact.displayName = firstname + " " + lastname;
-		
-		var name = new ContactName();
-		name.givenName = firstname;
-		name.familyName = lastname;
-		contact.name = name;
-		
-		var phoneNumbers = [new ContactField('mobile', phone,false)];
-		contact.phoneNumbers = phoneNumbers;
-		
-		var emails = [new ContactField('email', email,false)];
-		contact.emails = emails;
-		
-		var photos = [new ContactField('photo', img,false)];
-		contact.photos = photos;
-		
-		var organizations = [new ContactOrganization({name:'SCK Webworks'})]
-		contact.organizations = organizations;
-		
-		contact.save(function() {
-			addLink.unbind('click').text('Added').addClass('ui-disabled');
-		},fail);
-		return false;
-	});
-}
-
-var contacttemplate = '<div id="{{firstname}}{{lastname}}" data-lastname="{{lastname}}" data-firstname="{{firstname}}" data-phone="{{phone}}" data-email="{{email}}" class="page"><div class="container"><div class="header"><a href="#navmenu-panel" class="bars"></a><h1>{{firstname}} {{lastname}}</h1></div><div class="content"><div class="row"><div><div class="image_cell"><div><img src="images/{{img}}" alt="{{firstname}} {{lastname}}" /></div></div><div><h2>{{position}}</h2><p>{{description}}</p><div class="actions"><a href="{{url}}" class="btn">More about {{firstname}}</a></div></div></div></div></div></div></div>';
-var listtemplate = '<li><a href="#{{firstname}}{{lastname}}" class="pagelink">{{firstname}} {{lastname}}<br /><em>{{position}}</em></a></li>';
+var newstemplate = '<div id="news{{id}}" class="page"><div class="container"><div class="content"><div class="page_content"><h1>{{title}}</h1><p><em>{{date}}</em></p>{{content}}<p><a href="javascript:history.back();" class="backbtn"><span></span>Back to News</a></div></div></div></div>';
+var listtemplate = '<li class="pagelink"><a href="#news{{id}}" class="pagelink"><strong>{{title}}</strong><br />{{summary}}<br /><em>{{date}}</em></a></li>';
 function applyTemplate(temp, data) {
 	var newText = temp;
 	for(var nme in data) {
@@ -307,94 +244,5 @@ function applyTemplate(temp, data) {
 	return newText;
 }
 
-function getBase64Image(img) {
-    // Create an empty canvas element
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
 
-    // Copy the image contents to the canvas
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
-
-var defaults = [
-	{
-		firstname:'Graham',
-		lastname:'Sowerby',
-		position:'Director',
-		description:'Graham looks after business development and oversees search engine optimisation and digital marketing strategies for clients.',
-		url:'http://www.sck-webworks.co.uk/about-us/graham-sowerby',
-		img:'graham-sowerby-sck.jpg',
-		phone:'07545915222',
-		email:'graham@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Charlotte',
-		lastname:'Metcalfe',
-		position:'Accounts Manager',
-		description:'Charlotte manages SCK from day to day, and acts on behalf of the customer to ensure projects and service are delivered.',
-		url:'http://www.sck-webworks.co.uk/about-us/charlotte-metcalfe',
-		img:'charlotte-metcalfe-sck.jpg',
-		phone:'07810135884',
-		email:'charlotte@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Gavin',
-		lastname:'Cummings',
-		position:'Project Manager / Web Developer',
-		description:'Gavin is a dynamic database specialist, ensuring SCK websites manage customer and product data cleanly and efficiently.',
-		url:'http://www.sck-webworks.co.uk/about-us/gavin-cummings',
-		img:'gavin-cummings-sck.jpg',
-		phone:'01539722712',
-		email:'gavin@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Adam',
-		lastname:'Hewartson',
-		position:'Front End Developer',
-		description:'Adam uses the latest interaction methodology to ensure the customer user experience on all SCK websites is straightforward and engaging.',
-		url:'http://www.sck-webworks.co.uk/about-us/adam-hewartson',
-		img:'adam-hewartson-sck.jpg',
-		phone:'01539722712',
-		email:'adam@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Graham',
-		lastname:'Daws',
-		position:'PHP Developer / Server Manager',
-		description:'Graham\'s wealth of experience means the fundamental technical development of websites is of the highest quality.',
-		url:'http://www.sck-webworks.co.uk/about-us/graham-daws',
-		img:'graham-daws-sck.jpg',
-		phone:'01539722712',
-		email:'gdaws@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Adam',
-		lastname:'Ellison',
-		position:'Graphics Designer',
-		description:'Adam is a skilled graphic designer, and takes concepts from start to high quality finish, ensuring the end product looks great and fits perfectly.',
-		url:'http://www.sck-webworks.co.uk/about-us/adam-ellison',
-		img:'adam-ellison-sck.jpg',
-		phone:'01539722712',
-		email:'adam.ellison@sck-webworks.co.uk'
-	},
-	{
-		firstname:'Jack',
-		lastname:'Leddy',
-		position:'Apprentice',
-		description:'Jack is undertaking an IT Professional Apprenticeship at Kendal College whilst working with SCK.',
-		url:'http://www.sck-webworks.co.uk/about-us/jack-leddy',
-		img:'jack-leddy-sck.jpg',
-		phone:'01539722712',
-		email:'jack@sck-webworks.co.uk'
-	}
-];
+var defaults = [];
